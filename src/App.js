bc6123272507166eab757c749d7464723342dbd5
amrit073 <amritpaudel073@gmail.com>
2022-05-02 18:13:43 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+const Mid = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    await fetch('https://njpostblog.herokuapp.com/forreact')
+      .then(data => data.json())
+      .then(users => setUsers(users))
+      .catch(err => console.log(err))
+    // console.log(res)
+    // const resj = await resj.json()
+    // await setUsers(resj)
+  }
+  useEffect(() => {
+    console.log('use effect called')
+    getUsers()
+  }, []);
+  return <>
+    <h1> HI </h1>
+    {
+      users.map((user) => {
+        return (
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <section className='card text-white bg-secondary mb-3'>
+
+            <h5 className="card-header">
+              {user.title}
+            </h5>
+            <div className="card-body" key={user._id}>
+              <p className='card-title'>
+                {user.body}
+              </p>
+
+
+              <p className="card-text">
+                by: {user.name}
+              </p>
+                    
+            </div>
+          </section>
+        )
+      })
+    }
+  </>
+
 }
 
-export default App;
+export default Mid
